26e8c7c1e015b50778c86b536b5a622a5a9c0aa6
João Pedro Alves <jotalmeida007@hotmail.com>
2021-04-05 12:41:28 -0300

    Update script
    install postgresql

--- elixir-workspace-install.sh
+++ elixir-workspace-install.sh
@@ -25,3 +25,11 @@ asdf global erlang 23.3
 asdf local erlang 23.3
 
 mix archive.install hex phx_new
+
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+
+sudo apt-get update
+
+sudo apt-get -y install postgresql
