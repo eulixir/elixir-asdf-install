254292a62de66b61a04f3a47b352827250a24b80
João Pedro Alves <jotalmeida007@hotmail.com>
2021-12-11 00:12:09 -0300

    chore: remove locally postgresql install

--- elixir-workspace-install.sh
+++ elixir-workspace-install.sh
@@ -5,14 +5,6 @@ git clone https://github.com/asdf-vm/asdf.git ~/.asdf
 cd ~/.asdf
 git checkout "$(git describe --abbrev=0 --tags)"
 
-sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
-
-wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
-
-sudo apt-get update
-
-sudo apt-get -y install postgresql
-
 echo -e '\n. $HOME/.asdf/asdf.sh' >> ~/.bashrc
 echo -e '\n. $HOME/.asdf/completions/asdf.bash' >> ~/.bashrc
 source ~/.bashrc
